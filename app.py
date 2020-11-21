e28ae8ac988bacdeb1561f1a82c6df1b3d40d636
RealdoBeja98 <realdobeja@gmail.com>
2020-11-21 19:38:31 +0100

    Registration

--- app.py
+++ app.py
@@ -2,11 +2,40 @@ from flask import Flask, render_template, request, url_for, flash, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from forms import RegistrationForm, LoginForm
-
-
-
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from datetime import datetime
+ 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '493753003b7f9dc144cf6de900193330'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+bcrypt = Bcrypt(app)
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60),nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow )
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+        
 
 
 posts = [
@@ -40,8 +69,12 @@ def about():
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for { form.username.data }!', 'success')
-        return redirect(url_for('home'))
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 @app.route('/login')
--- forms.py
+++ forms.py
@@ -2,6 +2,9 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+
+
+
 class RegistrationForm(FlaskForm):                  
     username = StringField('Username', 
                                 validators=[DataRequired(), Length(min=2, max=20)])  #Username as a label    
